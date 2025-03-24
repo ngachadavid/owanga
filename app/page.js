1351d096b7e9e948410a69c808e0d7c3feeb5945
David Ngacha <david.ngacha@nathandigital.com>
2025-03-24 21:42:41 +0300

    two highlighted works

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import About from '@/components/homepage/About'
 import Hero from '@/components/homepage/Hero'
 import Logos from '@/components/homepage/Logos'
 import Skills from '@/components/homepage/Skills'
+import Works from '@/components/homepage/Works'
 import React from 'react'
 
 export default function Home () {
@@ -13,6 +14,7 @@ export default function Home () {
       <About />
       <Skills />
       <Logos />
+      <Works />
        </div>
   )
 }
--- components/homepage/Works.jsx
+++ components/homepage/Works.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 export default function Works() {
   return (
-    <div>Works</div>
-  )
+    <section className="bg-black py-20">
+    
+    <div className='bg-white py-20 mx-auto text-center'>
+        <h2 className='text-purple-500 text-6xl font-extrabold'>
+        Branding
+        </h2>
+
+    </div>
+      <div className="">
+        <img 
+          src="/works/citrica.png" 
+          alt="Branding showcase" 
+          className="w-full h-screen"
+        />
+      </div>
+      <div className="mt-40">
+        <img 
+          src="/works/dante.png" 
+          alt="Branding showcase" 
+          className="w-full h-auto"
+        />
+      </div>
+    </section>
+  );
 }
