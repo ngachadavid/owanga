8b9f2543de16dd2a6d4d4248cc1d059db9492ce4
David Ngacha <david.ngacha@nathandigital.com>
2025-03-27 20:42:41 +0300

    logo size adjustments

--- components/homepage/Logos.jsx
+++ components/homepage/Logos.jsx
@@ -12,14 +12,14 @@ export default function Logos() {
 
         {/* Logos Grid */}
         <div className="flex flex-wrap justify-center items-center gap-10 md:gap-32">
-          <img src="/logos/barfy.png" alt="Logo 1" className="h-16 md:h-20 w-auto" />
-          <img src="/logos/logo2.png" alt="Logo 2" className="h-16 md:h-20 w-auto" />
-          <img src="/logos/logo3.png" alt="Logo 3" className="h-16 md:h-20 w-auto" />
-          <img src="/logos/logo4.png" alt="Logo 4" className="h-16 md:h-20 w-auto" />
+          <img src="/logos/barfy.png" alt="Logo 1" className="h-16 md:h-24 w-auto" />
+          <img src="/logos/logo2.png" alt="Logo 2" className="h-16 md:h-26 w-auto" />
+          <img src="/logos/logo3.png" alt="Logo 3" className="h-16 md:h-30 w-auto" />
+          <img src="/logos/logo4.png" alt="Logo 4" className="h-16 md:h-16 w-auto" />
           <img src="/logos/logo5.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
           <img src="/logos/logo6.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
-          <img src="/logos/logo7.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
           <img src="/logos/logo8.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
+          <img src="/logos/logo7.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
           <img src="/logos/logo9.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
           <img src="/logos/logo10.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
           <img src="/logos/logo11.png" alt="Logo 5" className="h-16 md:h-20 w-auto" />
